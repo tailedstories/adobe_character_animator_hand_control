5b8a7085b3111dba2803b23ccd7df391089df343
tailedstories <81046709+tailedstories@users.noreply.github.com>
2021-05-28 18:11:37 -0700

    Implemented rtmidi, improved performance

--- body_control.py
+++ body_control.py
@@ -11,6 +11,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+import rtmidi
+
 
 def remap(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -18,11 +20,13 @@ def remap(x, in_min, in_max, out_min, out_max):
 def main():
 
     driver = webdriver.Firefox()
-    driver2 = webdriver.Firefox()
     
+    midiout = rtmidi.MidiOut()
+    available_ports = midiout.get_ports()
+    midiout.open_port(1)
         
     driver.get("file:///D:/unreal/body/body_tacker.html")
-    driver2.get("http://localhost:8080")
+    
     my_thumb = ""
     
     while my_thumb == "":
@@ -46,18 +50,20 @@ def main():
         
         if driver.find_element_by_id("leftarm").text == "Right In":
             if x != 1:
-                #x=1
-                driver2.find_element_by_xpath("//inner/div").click()
+                x=1
+                midiout.send_message([0x90, 81, 100])
+                #driver2.find_element_by_xpath("//inner/div").click()
                 print ("Right In")
-                time.sleep(0.2)
+                #time.sleep(0.1)
                 
                 #time.sleep(5)
         if driver.find_element_by_id("leftarm").text == "Right Out":
             if x != 2:
-                #x=2
-                driver2.find_element_by_xpath("//inner/div[2]").click()
+                x=2
+                midiout.send_message([0x90, 80, 100])
+                #driver2.find_element_by_xpath("//inner/div[2]").click()
                 print ("Right Out")
-                time.sleep(0.2)
+                #time.sleep(0.1)
                 #time.sleep(5)
         if int(driver.find_element_by_id("wrist").text) < 165 and int(driver.find_element_by_id("wrist").text) > 0:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
@@ -66,23 +72,25 @@ def main():
                 NewValue = remap(int(driver.find_element_by_id("wrist").text), 0, 165, 62, 127)
 
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
+                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
+                midiout.send_message([176, 20, int(NewValue)])
                 print (NewValue)
-                time.sleep(0.2)
+                time.sleep(0.1)
         if int(driver.find_element_by_id("wrist").text) < 0 and int(driver.find_element_by_id("wrist").text) > -90:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
                 NewValue = remap(abs(int(driver.find_element_by_id("wrist").text)), 0, 90, 31, 61)
                 
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-                print (tmp_ind2)
-                time.sleep(0.2)
+                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
+                midiout.send_message([176, 20, int(NewValue)])
+                #print (tmp_ind2)
+                time.sleep(0.1)
                 
         if int(driver.find_element_by_id("wrist").text) < 270 and int(driver.find_element_by_id("wrist").text) > 179:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
@@ -90,11 +98,12 @@ def main():
                 NewValue = remap(int(driver.find_element_by_id("wrist").text), 180, 270, 0, 30)
                 
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-                print (tmp_ind2)
+                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
+                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
+                #print (tmp_ind2)
+                midiout.send_message([176, 20, int(NewValue)])
                 time.sleep(0.2)
                 
                 #time.sleep(5)
@@ -109,12 +118,13 @@ def main():
             NewValue = remap(int(driver.find_element_by_id("coord").text)+180, 0, 360, 0, 127)
             
             tmp_ind = int(driver.find_element_by_id("coord").text)
-            driver2.find_elements_by_xpath("//div[10]/inner/canvas")[0].click()
-            driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-            driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-            driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-            print (int(NewValue))
-            time.sleep(0.2)
+            #driver2.find_elements_by_xpath("//div[10]/inner/canvas")[0].click()
+            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
+            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
+            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
+            #print (int(NewValue))
+            midiout.send_message([176, 21, int(NewValue)])
+            time.sleep(0.1)
         
         
         
