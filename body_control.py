a08049ca82ca9cc9859845fe71585acf740bb130
tailedstories <81046709+tailedstories@users.noreply.github.com>
2021-05-29 07:27:44 -0700

    Fixed some wrong elbow angles

--- body_control.py
+++ body_control.py
@@ -48,41 +48,41 @@ def main():
         if driver.find_element_by_id("leftarm").text == "Right In":
             if x != 1:
                 x=1
-                midiout.send_message([0x90, 81, 100])
+                midiout.send_message([0x90, 80, 100])
                 print ("Right In")
                 #time.sleep(0.1)
         if driver.find_element_by_id("leftarm").text == "Right Out":
             if x != 2:
                 x=2
-                midiout.send_message([0x90, 80, 100])
+                midiout.send_message([0x90, 81, 100])
                 print ("Right Out")                
         if int(driver.find_element_by_id("wrist").text) < 165 and int(driver.find_element_by_id("wrist").text) > 0:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:
                 NewValue = remap(int(driver.find_element_by_id("wrist").text), 0, 165, 62, 127)
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
                 midiout.send_message([176, 21, int(NewValue)])    
-                time.sleep(0.1)
+                #time.sleep(0.1)
                 #print ("Elbow - ", NewValue)
         if int(driver.find_element_by_id("wrist").text) < 0 and int(driver.find_element_by_id("wrist").text) > -90:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:
-                NewValue = remap(abs(int(driver.find_element_by_id("wrist").text)), 0, 90, 31, 61)
+                NewValue = remap(int(driver.find_element_by_id("wrist").text)+90, 0, 90, 31, 61)
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
                 midiout.send_message([176, 21, int(NewValue)])
-                time.sleep(0.1)
+                #time.sleep(0.1)
                 #print ("Elbow - ", NewValue)
         if int(driver.find_element_by_id("wrist").text) < 270 and int(driver.find_element_by_id("wrist").text) > 179:
             if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:                
                 NewValue = abs(remap(int(driver.find_element_by_id("wrist").text), 180, 270, 0, 30))
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
                 midiout.send_message([176, 21, int(NewValue)])
-                time.sleep(0.1)
+                #time.sleep(0.1)
                 #print ("Elbow - ", NewValue)
         if abs(tmp_ind - int(driver.find_element_by_id("coord").text)) > 5:
             NewValue = remap(int(driver.find_element_by_id("coord").text)+180, 0, 360, 0, 127)
             tmp_ind  = int(driver.find_element_by_id("coord").text)
             midiout.send_message([176, 20, int(NewValue)])
-            print ("Shoulder - ", int(NewValue))
-            time.sleep(0.1)
+            #print ("Shoulder - ", int(NewValue))
+            #time.sleep(0.1)
         
         
     del midiout
