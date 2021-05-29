01b6896f2347cb2b9400021a6d18058cfbdaa37b
tailedstories <81046709+tailedstories@users.noreply.github.com>
2021-05-29 07:19:36 -0700

    Cleaned code fixed References

--- body_control.py
+++ body_control.py
@@ -37,10 +37,7 @@ def main():
          my_thumb = driver.find_element_by_id("leftarm").text
          time.sleep(1)
 
-    #driver2.find_elements_by_xpath('//inner/input').send_keys(100)
-    #driver2.find_elements_by_xpath("//div[5]/inner").send_keys()
-    
-    
+       
     
     print (my_thumb)
     x=0
@@ -52,82 +49,43 @@ def main():
             if x != 1:
                 x=1
                 midiout.send_message([0x90, 81, 100])
-                #driver2.find_element_by_xpath("//inner/div").click()
                 print ("Right In")
                 #time.sleep(0.1)
-                
-                #time.sleep(5)
         if driver.find_element_by_id("leftarm").text == "Right Out":
             if x != 2:
                 x=2
                 midiout.send_message([0x90, 80, 100])
-                #driver2.find_element_by_xpath("//inner/div[2]").click()
-                print ("Right Out")
-                #time.sleep(0.1)
-                #time.sleep(5)
+                print ("Right Out")                
         if int(driver.find_element_by_id("wrist").text) < 165 and int(driver.find_element_by_id("wrist").text) > 0:
-            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
-
-                #(x, in_min, in_max, out_min, out_max):
+            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:
                 NewValue = remap(int(driver.find_element_by_id("wrist").text), 0, 165, 62, 127)
-
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-                midiout.send_message([176, 20, int(NewValue)])
-                print (NewValue)
+                midiout.send_message([176, 21, int(NewValue)])    
                 time.sleep(0.1)
+                #print ("Elbow - ", NewValue)
         if int(driver.find_element_by_id("wrist").text) < 0 and int(driver.find_element_by_id("wrist").text) > -90:
-            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
+            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:
                 NewValue = remap(abs(int(driver.find_element_by_id("wrist").text)), 0, 90, 31, 61)
-                
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-                midiout.send_message([176, 20, int(NewValue)])
-                #print (tmp_ind2)
+                midiout.send_message([176, 21, int(NewValue)])
                 time.sleep(0.1)
-                
+                #print ("Elbow - ", NewValue)
         if int(driver.find_element_by_id("wrist").text) < 270 and int(driver.find_element_by_id("wrist").text) > 179:
-            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 10:
-                
-                NewValue = remap(int(driver.find_element_by_id("wrist").text), 180, 270, 0, 30)
-                
+            if abs(tmp_ind2 - int(driver.find_element_by_id("wrist").text)) > 5:                
+                NewValue = abs(remap(int(driver.find_element_by_id("wrist").text), 180, 270, 0, 30))
                 tmp_ind2 = int(driver.find_element_by_id("wrist").text)
-                #driver2.find_elements_by_xpath("//div[11]/inner/canvas")[0].click()
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-                #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-                #print (tmp_ind2)
-                midiout.send_message([176, 20, int(NewValue)])
-                time.sleep(0.2)
-                
-                #time.sleep(5)
-            #if x != 3:
-                #x=3
-                #0-160 --> 40-210 ==> -50
-        #driver2.find_elements_by_xpath("//div[10]/inner/canvas")[0].click()
-        #driver2.find_elements_by_xpath("//inner/input").clear()
-        #time.sleep(0.2)
+                midiout.send_message([176, 21, int(NewValue)])
+                time.sleep(0.1)
+                #print ("Elbow - ", NewValue)
         if abs(tmp_ind - int(driver.find_element_by_id("coord").text)) > 5:
-            
             NewValue = remap(int(driver.find_element_by_id("coord").text)+180, 0, 360, 0, 127)
-            
-            tmp_ind = int(driver.find_element_by_id("coord").text)
-            #driver2.find_elements_by_xpath("//div[10]/inner/canvas")[0].click()
-            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.CONTROL + "a")
-            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(int(NewValue))
-            #driver2.find_elements_by_xpath("//inner/input")[0].send_keys(Keys.ENTER)
-            #print (int(NewValue))
-            midiout.send_message([176, 21, int(NewValue)])
+            tmp_ind  = int(driver.find_element_by_id("coord").text)
+            midiout.send_message([176, 20, int(NewValue)])
+            print ("Shoulder - ", int(NewValue))
             time.sleep(0.1)
         
         
-        
+    del midiout
         #time.sleep(1)
         
 if __name__ == "__main__":
