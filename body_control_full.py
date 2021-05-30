072112815ac9d69df6138e74d4a63aaa76b63611
tailedstories <81046709+tailedstories@users.noreply.github.com>
2021-05-30 13:10:01 -0700

    Both hands work in real time

--- body_control_full.py
+++ body_control_full.py
@@ -23,7 +23,23 @@ midiout.open_port(1)
 
 
 
-
+def prepangle(x1,y1,x2,y2,x3,y3):
+    a1 = myAngle(x1,y1,x2,y2)
+    a2 = myAngle(x2,y2,x3,y3)
+    
+    if a1 > a2 :
+       sign =  1
+    else:
+       sign = -1
+    
+    result_angle = a1 - a2
+    K = -sign * math.pi * 2
+    
+    if (abs(K + result_angle) < abs(result_angle)): 
+        result_angle = K + result_angle
+    else:
+        result_angle = result_angle
+    return round(result_angle)
 
 def remap(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -37,10 +53,12 @@ def myAngle(cx, cy, ex, ey):
 
 
 # For webcam input:
-cap = cv2.VideoCapture(3)
+cap = cv2.VideoCapture(1)
 x=0
 tmp_ind=0
 tmp_ind2=0
+tmp_ind_r=0
+tmp_ind_l=0
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
@@ -67,68 +85,122 @@ with mp_pose.Pose(
     cv2.imshow('MediaPipe Pose', image)
     
     keypoints = []
-    for data_point in results.pose_landmarks.landmark:
-        keypoints.append({
-                             'X': data_point.x,
-                             'Y': data_point.y,
-                             'Z': data_point.z,
-                             'Visibility': data_point.visibility,
-                             })
-    
-    if len(keypoints) > 16:
-        #print("1 - ",keypoints[12].get("X")," 2 - ",keypoints[12].get("Y")," 3 - ",keypoints[14].get("X")," 4 - ",keypoints[14].get("Y"))
-        my_shoulder = myAngle(keypoints[12].get("X"),keypoints[12].get("Y"),keypoints[14].get("X"),keypoints[14].get("Y"))
-        #print("Right Shoulder : ", my_shoulder)
+    if results.pose_landmarks != None:
+        for data_point in results.pose_landmarks.landmark:
+            keypoints.append({
+                                 'X': data_point.x,
+                                 'Y': data_point.y,
+                                 'Z': data_point.z,
+                                 'Visibility': data_point.visibility,
+                                 })
         
-        a1 = myAngle(keypoints[12].get("X"),keypoints[12].get("Y"),keypoints[14].get("X"),keypoints[14].get("Y"))
-        a2 = myAngle(keypoints[14].get("X"),keypoints[14].get("Y"),keypoints[16].get("X"),keypoints[16].get("Y"))
-        
-        if a1 > a2 :
-           sign =  1
-        else:
-           sign = -1
-        
-        result_angle = a1 - a2
-        K = -sign * math.pi * 2
-        
-        if (abs(K + result_angle) < abs(result_angle)): 
-            result_angle = K + result_angle
-        else:
-            result_angle = result_angle
-        
-        #print("Right Elbow : ",round(result_angle))
-        my_elbow = round(result_angle)
-        
-        
-        if int(my_shoulder) < 165 and int(my_shoulder) > 0:
-            if abs(tmp_ind2 - int(my_shoulder)) > 2:
-                NewValue = remap(int(my_shoulder), 0, 165, 62, 127)
-                tmp_ind2 = int(my_shoulder)
-                midiout.send_message([176, 21, int(NewValue)])    
+        if len(keypoints) > 16:
+            #print("1 - ",keypoints[12].get("X")," 2 - ",keypoints[12].get("Y")," 3 - ",keypoints[14].get("X")," 4 - ",keypoints[14].get("Y"))
+            my_shoulder_r = myAngle(keypoints[12].get("X"),keypoints[12].get("Y"),keypoints[14].get("X"),keypoints[14].get("Y"))
+            
+            my_shoulder_l = myAngle(keypoints[11].get("X"),keypoints[11].get("Y"),keypoints[13].get("X"),keypoints[13].get("Y"))
+            #print("Right Shoulder : ", my_shoulder)
+            
+            my_elbow_r = prepangle(keypoints[12].get("X"),keypoints[12].get("Y"),
+                                   keypoints[14].get("X"),keypoints[14].get("Y"),
+                                   keypoints[16].get("X"),keypoints[16].get("Y"))
+            
+            my_elbow_l = prepangle(keypoints[11].get("X"),keypoints[11].get("Y"),
+                                   keypoints[13].get("X"),keypoints[13].get("Y"),
+                                   keypoints[15].get("X"),keypoints[15].get("Y"))
+            
+            
+            #right elbow
+            if int(my_elbow_r) < 165 and int(my_elbow_r) > 0:
+                if abs(tmp_ind_r - int(my_elbow_r)) > 2:
+                    NewValue = remap(int(my_elbow_r), 0, 165, 62, 127)
+                    tmp_ind_r = int(my_elbow_r)
+                    midiout.send_message([176, 21, int(NewValue)])    
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+            if int(my_elbow_r) < 0 and int(my_elbow_r) > -90:
+                if abs(tmp_ind_r - int(my_elbow_r)) > 2:
+                    NewValue = remap(int(my_elbow_r)+90, 0, 90, 31, 61)
+                    tmp_ind_r = int(my_elbow_r)
+                    midiout.send_message([176, 21, int(NewValue)])
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+            if int(my_elbow_r) < 270 and int(my_elbow_r) > 179:
+                if abs(tmp_ind_r - int(my_elbow_r)) > 2:                
+                    NewValue = abs(remap(int(my_elbow_r), 180, 270, 0, 30))
+                    tmp_ind_r = int(my_elbow_r)
+                    midiout.send_message([176, 21, int(NewValue)])
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+                    
+            #left elbow
+            if int(my_elbow_l) < 165 and int(my_elbow_l) > 0:
+                if abs(tmp_ind_l - int(my_elbow_l)) > 2:
+                    NewValue = remap(int(my_elbow_l), 0, 165, 62, 127)
+                    tmp_ind_l = int(my_elbow_l)
+                    midiout.send_message([176, 18, int(NewValue)])    
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+            if int(my_elbow_l) < 0 and int(my_elbow_l) > -90:
+                if abs(tmp_ind_l - int(my_elbow_l)) > 2:
+                    NewValue = remap(int(my_elbow_l)+90, 0, 90, 31, 61)
+                    tmp_ind_l = int(my_elbow_l)
+                    midiout.send_message([176, 18, int(NewValue)])
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+            if int(my_elbow_l) < 270 and int(my_elbow_l) > 179:
+                if abs(tmp_ind_l - int(my_elbow_l)) > 2:                
+                    NewValue = abs(remap(int(my_elbow_l), 180, 270, 0, 30))
+                    tmp_ind_l = int(my_elbow_l)
+                    midiout.send_message([176, 18, int(NewValue)])
+                    #time.sleep(0.1)
+                    #print ("Elbow - ", NewValue)
+            
+            
+            
+            #right shoulder 
+            if abs(tmp_ind - int(my_shoulder_r)) > 2:
+                NewValue = remap(int(my_shoulder_r)+180, 0, 360, 0, 127)
+                tmp_ind  = int(my_shoulder_r)
+                midiout.send_message([176, 20, int(NewValue)])
+                #print ("Shoulder right - ", int(NewValue))
                 #time.sleep(0.1)
-                print ("Elbow - ", NewValue)
-        if int(my_shoulder) < 0 and int(my_shoulder) > -90:
-            if abs(tmp_ind2 - int(my_shoulder)) > 2:
-                NewValue = remap(int(my_shoulder)+90, 0, 90, 31, 61)
-                tmp_ind2 = int(my_shoulder)
-                midiout.send_message([176, 21, int(NewValue)])
-                #time.sleep(0.1)
-                print ("Elbow - ", NewValue)
-        if int(my_shoulder) < 270 and int(my_shoulder) > 179:
-            if abs(tmp_ind2 - int(my_shoulder)) > 2:                
-                NewValue = abs(remap(int(my_shoulder), 180, 270, 0, 30))
-                tmp_ind2 = int(my_shoulder)
-                midiout.send_message([176, 21, int(NewValue)])
-                #time.sleep(0.1)
-                print ("Elbow - ", NewValue)
                 
-        if abs(tmp_ind - int(my_elbow)) > 2:
-            NewValue = remap(int(my_elbow)+180, 0, 360, 0, 127)
-            tmp_ind  = int(my_elbow)
-            midiout.send_message([176, 20, int(NewValue)])
-            print ("Shoulder - ", int(NewValue))
-            #time.sleep(0.1)
-    
+            #left shoulder 
+            if abs(tmp_ind2 - int(my_shoulder_l)) > 2:
+                NewValue = abs(remap(int(my_shoulder_l)+180, 0, 360, 0, 127)-127)
+                
+                tmp_ind2  = int(my_shoulder_l)
+                midiout.send_message([176, 17, int(NewValue)])
+                #print ("Shoulder left - ", int(NewValue))
+                #time.sleep(0.1)
+            
+            
+            
+            #right wrist auto flip
+            if(keypoints[16].get("X") > keypoints[12].get("X")) :
+                if x != 1:
+                    x=1
+                    midiout.send_message([0x90, 80, 100])
+                    #print ("Right In")
+            else:
+                if x != 2:
+                    x=2
+                    midiout.send_message([0x90, 81, 100])
+                    #print ("Right Out")
+                    
+            #left wrist auto flip
+            if(keypoints[15].get("X") > keypoints[11].get("X")) :
+                if x != 1:
+                    x=1
+                    midiout.send_message([0x90, 83, 100])
+                    #print ("Left In")
+            else:
+                if x != 2:
+                    x=2
+                    midiout.send_message([0x90, 82, 100])
+                    #print ("Left Out")
+        
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
